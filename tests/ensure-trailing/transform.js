a951efc99ed33f8414c8c7de6d2e30553e571539
Samy Pessé <samypesse@gmail.com>
2016-11-03 18:02:05 +0100

    Adapt unit tests

--- tests/ensure-trailing/transform.js
+++ tests/ensure-trailing/transform.js
@@ -2,6 +2,8 @@ const Slate = require('slate');
 
 module.exports = function(plugin, state) {
     const schema = new Slate.Schema(plugin.schema);
-    const normalized = state.normalize(schema);
+    const normalized = state.transform()
+        .normalizeWith(schema)
+        .apply();
     return normalized;
 };
